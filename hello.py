193adfcfef9471b83f00aad8ae2e45188c7afc60
Jimmy Ye <jmy@gmail.com>
2021-08-11 23:58:29 +0000

    adding multiplication

--- hello.py
+++ hello.py
@@ -11,3 +11,10 @@ def add(x_0, y_0):
 
 RESULT = add(1, 2)
 print(f"1, 2, {RESULT}")
+
+def mul(x_0, y_0):
+    """ Multiplication Function"""
+    return x_0 * y_0
+
+resultm = mul(4,5)
+print(f"Multiplication 4 x 5 = {resultm}")
