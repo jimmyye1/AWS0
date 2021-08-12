4045e0859d32f4101ecbd3f1b65b6cde93939e3e
Jimmy Ye <jimmy.ye@gmail.com>
2021-08-12 12:28:31 +0000

    Add Subtraction

--- hello.py
+++ hello.py
@@ -19,3 +19,12 @@ def mul(x_0, y_0):
 NEW = mul(4, 5)
 
 print(f"Multiplication 4 x 5 = {NEW}")
+
+def sub(x_0, y_0):
+    """Subtraction Function"""
+    return x_0 - y_0
+
+X_1 = 20
+Y_1 = 4
+NEW_RESULT = sub(X_1, Y_1)
+print(f"subtraction {X_1} - {Y_1} = {NEW_RESULT}")
